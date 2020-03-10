3473762ee2eb960e989731c457f078632029220e
joecooper1 <joecooper2991@gmail.com>
2020-03-10 11:17:57 +0000

    binary search kata compelete

--- __tests__/binarySearch.test.js
+++ __tests__/binarySearch.test.js
@@ -1,7 +1,35 @@
-const { binarySearch } = require('../binarySearch')
+const { binarySearch } = require("../binarySearch");
 
-describe('binarySearch()', () => {
-  it('', () => {
-    // first test here
-  })
-})
+describe("binarySearch()", () => {
+  it("uses binary search to find index of a number in a small, sorted array", () => {
+    expect(binarySearch(1, [1, 2, 3, 4, 5])).toBe(0);
+    expect(binarySearch(4, [1, 2, 3, 4, 5])).toBe(3);
+    expect(
+      binarySearch(14, [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20
+      ])
+    ).toBe(13);
+  });
+  it("works on arrays of non-consecutive numbers", () => {
+    expect(binarySearch(7, [2, 4, 5, 7, 9, 12, 11, 13, 189])).toBe(3);
+  });
+});
--- binarySearch.js
+++ binarySearch.js
@@ -1,6 +1,19 @@
 // Please do not change the name of this function
-const binarySearch = (arr) => {
-  // Your code here
-}
+const binarySearch = (num, arr, pagesChecked = 0) => {
+  const indexToCheck = Math.floor(arr.length / 2);
+  const numToCheck = arr[indexToCheck];
 
-module.exports = { binarySearch }
+  if (numToCheck === num) {
+    return indexToCheck + pagesChecked;
+  } else if (numToCheck > num) {
+    return binarySearch(num, arr.slice(0, indexToCheck), pagesChecked);
+  } else if (num > numToCheck) {
+    return binarySearch(
+      num,
+      arr.slice(indexToCheck + 1, arr.length),
+      pagesChecked + indexToCheck + 1
+    );
+  }
+};
+
+module.exports = { binarySearch };
