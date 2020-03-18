8034c5cdb81a2f79d0935894a5614475e6c18ab8
joecooper1 <joecooper2991@gmail.com>
2020-03-18 16:59:40 +0000

    solved rgbaModifier

--- __tests__/rgbaModifier.test.js
+++ __tests__/rgbaModifier.test.js
@@ -1,7 +1,35 @@
-const { rgbaModifier } = require('../rgbaModifier')
+const { rgbaModifier } = require("../rgbaModifier");
 
-describe('rgbaModifier()', () => {
-  it('', () => {
+describe("rgbaModifier()", () => {
+  it("does not change rgb values when arguments given are zero", () => {
     // first test here
-  })
-})
+    expect(rgbaModifier("rgba(100,100,100,0.6)", 0, 0.6)).toBe(
+      "rgba(100,100,100,0.6)"
+    );
+  });
+  it("changes the opacity to match the new value", () => {
+    expect(rgbaModifier("rgba(100,100,100,0.3)", 0, 0.7)).toBe(
+      "rgba(100,100,100,0.7)"
+    );
+  });
+  it("changes the rgb values by the percentage", () => {
+    expect(rgbaModifier("rgba(100,10,1,0.3)", 20, 0.7)).toBe(
+      "rgba(120,12,1,0.7)"
+    );
+    expect(rgbaModifier("rgba(100,10,1,0.3)", -20, 0.7)).toBe(
+      "rgba(80,8,1,0.7)"
+    );
+  });
+  it("changes values too low or too high to be the max or min allowed", () => {
+    expect(rgbaModifier("rgba(100,10,1,0.3)", 20, 5)).toBe("rgba(120,12,1,1)");
+    expect(rgbaModifier("rgba(100,10,1,0.3)", -20, -0.3)).toBe(
+      "rgba(80,8,1,0)"
+    );
+    expect(rgbaModifier("rgba(100,10,1,0.3)", 500, 0.5)).toBe(
+      "rgba(255,60,6,0.5)"
+    );
+    expect(rgbaModifier("rgba(100,10,1,0.3)", -200, 0.5)).toBe(
+      "rgba(0,0,0,0.5)"
+    );
+  });
+});
--- rgbaModifier.js
+++ rgbaModifier.js
@@ -1,6 +1,19 @@
 // Please do not change the name of this function
 const rgbaModifier = (rgbaStr, percent, opacity) => {
   // Your code here
-}
+  const rgbValues = [];
+  for (let i = 5; rgbValues.length < 3; ) {
+    let nextComma = rgbaStr.indexOf(",", i);
+    let value = Number(rgbaStr.slice(i, nextComma));
+    let newValue = value + Math.round(value * (percent / 100));
+    if (newValue > 255) newValue = 255;
+    if (newValue < 0) newValue = 0;
+    rgbValues.push(newValue);
+    i = nextComma + 1;
+  }
+  if (opacity > 1) opacity = 1;
+  if (opacity < 0) opacity = 0;
+  return `rgba(${rgbValues[0]},${rgbValues[1]},${rgbValues[2]},${opacity})`;
+};
 
-module.exports = { rgbaModifier }
+module.exports = { rgbaModifier };
