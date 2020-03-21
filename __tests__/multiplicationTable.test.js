2f71b350030237d44dacc20ad8415aeee6fd960b
joecooper1 <joecooper2991@gmail.com>
2020-03-21 11:13:26 +0000

    solved multiplication table

--- __tests__/multiplicationTable.test.js
+++ __tests__/multiplicationTable.test.js
@@ -1,7 +1,21 @@
-const { multiplicationTable } = require('../multiplicationTable')
+const { multiplicationTable } = require("../multiplicationTable");
 
-describe('multiplicationTable()', () => {
-  it('', () => {
-    // first test here
-  })
-})
+describe("multiplicationTable()", () => {
+  it("returns an empty array when given 0", () => {
+    expect(multiplicationTable(0)).toEqual([]);
+  });
+  it("returns an array of arrays when given a valid integer", () => {
+    expect(multiplicationTable(1)).toEqual([
+      [0, 0],
+      [1, 1]
+    ]);
+  });
+  it("returns an array of arrays when given a valid integer", () => {
+    expect(multiplicationTable(3)).toEqual([
+      [0, 0, 0, 0],
+      [1, 1, 2, 3],
+      [2, 2, 4, 6],
+      [3, 3, 6, 9]
+    ]);
+  });
+});
--- multiplicationTable.js
+++ multiplicationTable.js
@@ -1,6 +1,16 @@
 // Please do not change the name of this function
-const multiplicationTable = (num) => {
-  // Your code here
-}
+const multiplicationTable = num => {
+  if (num === 0) return [];
 
-module.exports = { multiplicationTable }
+  const newArray = [];
+  for (let i = 0; i < num + 1; i++) {
+    const subArray = [i];
+    for (let j = 1; j < num + 1; j++) {
+      subArray.push(j * i);
+    }
+    newArray.push(subArray);
+  }
+  return newArray;
+};
+
+module.exports = { multiplicationTable };
