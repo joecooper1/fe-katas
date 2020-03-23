b1ff6b4b00db1ba7cb9a5642bca4685983413446
joecooper1 <joecooper2991@gmail.com>
2020-03-23 20:59:13 +0000

    solved needleInHaystack

--- __tests__/needleInHaystack.test.js
+++ __tests__/needleInHaystack.test.js
@@ -1,7 +1,57 @@
-const { needleInHaystack } = require('../needleInHaystack')
+const { needleInHaystack } = require("../needleInHaystack");
 
-describe('needleInHaystack()', () => {
-  it('', () => {
-    // first test here
-  })
-})
+const haystack = {
+  name: "Northcoders",
+  description: "Awesome coding bootcamp",
+  staff: {
+    tutors: "James and Chris",
+    support: "Louise"
+  },
+  contactDetails: {
+    web: "https://northcoders.com",
+    phone: "",
+    address: {
+      office: "Northcoders, Gold 67, The Sharp Project, Manchester",
+      postcode: "M40 5BJ"
+    }
+  },
+  reviews: {
+    april: {
+      chris: "I love Northcoders",
+      james: "It is awesome!"
+    },
+    may: {
+      louise: "Northcoders is a fantastic coding bootcamp"
+    }
+  }
+};
+
+describe("needleInHaystack()", () => {
+  it("returns an empty array when the word does not appear", () => {
+    const needle = "Sausages";
+    expect(needleInHaystack(haystack, needle)).toEqual([]);
+  });
+  it("returns array of location of the needle", () => {
+    const needle = "M40";
+    expect(needleInHaystack(haystack, needle)).toEqual([
+      "contactDetails.address.postcode"
+    ]);
+  });
+  it("returns array of location of the needle", () => {
+    const needle = "coding";
+    expect(needleInHaystack(haystack, needle)).toEqual([
+      "description",
+      "reviews.may.louise"
+    ]);
+  });
+  it("is not case sensitive", () => {
+    const needle = "northCodeRs";
+    expect(needleInHaystack(haystack, needle)).toEqual([
+      "contactDetails.address.office",
+      "contactDetails.web",
+      "name",
+      "reviews.april.chris",
+      "reviews.may.louise"
+    ]);
+  });
+});
--- needleInHaystack.js
+++ needleInHaystack.js
@@ -1,6 +1,25 @@
 // Please do not change the name of this function
 const needleInHaystack = (haystack, needle) => {
-  // Your code here
-}
+  const needleLocations = [];
 
-module.exports = { needleInHaystack }
+  const checkObject = (object, needle, needleLocations, route = "") => {
+    for (let key in object) {
+      if (typeof object[key] === "string") {
+        const regex = RegExp(needle.toLowerCase(), "g");
+
+        if (regex.test(object[key].toLowerCase())) {
+          needleLocations.push(route + `${key}`);
+        } else {
+          ("");
+        }
+      } else {
+        checkObject(object[key], needle, needleLocations, route + `${key}.`);
+      }
+    }
+    return needleLocations;
+  };
+
+  return checkObject(haystack, needle, []).sort();
+};
+
+module.exports = { needleInHaystack };
