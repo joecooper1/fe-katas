e00e4920bf511069ad8f3e76dbbb16b0e44c84f4
joecooper1 <joecooper2991@gmail.com>
2020-04-17 13:42:50 +0100

    solved rotateMatrix

--- __tests__/rotateMatrix.test.js
+++ __tests__/rotateMatrix.test.js
@@ -1,7 +1,38 @@
-const { rotateMatrix } = require('../rotateMatrix')
+const { rotateMatrix } = require("../rotateMatrix");
 
-describe('rotateMatrix()', () => {
-  it('', () => {
-    // first test here
-  })
-})
+describe("rotateMatrix()", () => {
+  it("when given a single array with a single value, returns that same array", () => {
+    const matrix = [[1]];
+    const output = [[1]];
+    expect(rotateMatrix(matrix)).toEqual(output);
+  });
+  it("works for small, square arrays", () => {
+    const matrix = [
+      [1, 2, 3],
+      [1, 2, 3],
+      [1, 2, 3],
+    ];
+    const output = [
+      [1, 1, 1],
+      [2, 2, 2],
+      [3, 3, 3],
+    ];
+    expect(rotateMatrix(matrix)).toEqual(output);
+  });
+  it("when for rectangular arrays", () => {
+    const matrix = [
+      [1, 2, 3, 5, 10, 1],
+      [3, 3, 1, 1, 3, 6],
+      [1, 6, 3, 2, 2, 1],
+    ];
+    const output = [
+      [1, 3, 1],
+      [6, 3, 2],
+      [3, 1, 3],
+      [2, 1, 5],
+      [2, 3, 10],
+      [1, 6, 1],
+    ];
+    expect(rotateMatrix(matrix)).toEqual(output);
+  });
+});
--- rotateMatrix.js
+++ rotateMatrix.js
@@ -1,6 +1,14 @@
 // Please do not change the name of this function
 const rotateMatrix = (matrix) => {
-  // Your code here
-}
+  const newArray = [];
+  for (let i = 0; i < matrix.length; i++) {
+    let subMatrix = matrix[i];
+    for (let j = 0; j < subMatrix.length; j++) {
+      if (i === 0) newArray.push([]);
+      newArray[j].unshift(subMatrix[j]);
+    }
+  }
+  return newArray;
+};
 
-module.exports = { rotateMatrix }
+module.exports = { rotateMatrix };
