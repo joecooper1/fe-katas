2e85167476d8d6c92b0a93190b92b9623a3f6c86
joecooper1 <joecooper2991@gmail.com>
2020-04-21 12:15:36 +0100

    solved fillSquare

--- __tests__/fillSquare.test.js
+++ __tests__/fillSquare.test.js
@@ -1,7 +1,54 @@
-const { fillSquare } = require('../fillSquare')
+const { fillSquare } = require("../fillSquare");
 
-describe('fillSquare()', () => {
-  it('', () => {
-    // first test here
-  })
-})
+describe("fillSquare()", () => {
+  it("returns empty array when given empty array", () => {
+    const square = [];
+    const expected = [];
+    expect(fillSquare(square)).toEqual(expected);
+  });
+  it("returns same array when given a complete square", () => {
+    const square = [
+      [1, 2],
+      [1, 2],
+    ];
+    const expected = [
+      [1, 2],
+      [1, 2],
+    ];
+    expect(fillSquare(square)).toEqual(expected);
+  });
+  it("fills in empty spaces with null", () => {
+    const square = [[1, 2, 3], [1, 2], [1]];
+    const expected = [
+      [1, 2, 3],
+      [1, 2, null],
+      [1, null, null],
+    ];
+    expect(fillSquare(square)).toEqual(expected);
+  });
+  it("does not mutate", () => {
+    const square = [[1, 2, 3], [1, 2, 3, 4, 5, 6], [1]];
+    const expected = [
+      [1, 2, 3, null, null, null],
+      [1, 2, 3, 4, 5, 6],
+      [1, null, null, null, null, null],
+      [null, null, null, null, null, null],
+      [null, null, null, null, null, null],
+      [null, null, null, null, null, null],
+    ];
+    expect(fillSquare(square)).toEqual(expected);
+    expect(square).toEqual([[1, 2, 3], [1, 2, 3, 4, 5, 6], [1]]);
+  });
+  it("works for large arrays", () => {
+    const square = [[1, 2, 3], [1, 2, 3], [1], [], [1, 2, 3], [1]];
+    const expected = [
+      [1, 2, 3, null, null, null],
+      [1, 2, 3, null, null, null],
+      [1, null, null, null, null, null],
+      [null, null, null, null, null, null],
+      [1, 2, 3, null, null, null],
+      [1, null, null, null, null, null],
+    ];
+    expect(fillSquare(square)).toEqual(expected);
+  });
+});
--- fillSquare.js
+++ fillSquare.js
@@ -1,6 +1,30 @@
 // Please do not change the name of this function
 const fillSquare = (arr) => {
-  // Your code here
-}
+  let squareSize = arr.length;
+  const maxSubArrLength = (() => {
+    if (arr.length > 0) {
+      return [...arr].sort((a, b) => b.length - a.length)[0].length;
+    } else return 0;
+  })();
+  if (maxSubArrLength > arr.length) squareSize = maxSubArrLength;
 
-module.exports = { fillSquare }
+  const newArr = [];
+  for (let i = 0; i < squareSize; i++) {
+    newArr.push([]);
+    for (let j = 0; j < squareSize; j++) {
+      if (arr[i]) {
+        if (arr[i][j]) {
+          newArr[i].push(arr[i][j]);
+        } else {
+          newArr[i].push(null);
+        }
+      } else {
+        newArr[i].push(null);
+      }
+    }
+  }
+
+  return newArr;
+};
+
+module.exports = { fillSquare };
