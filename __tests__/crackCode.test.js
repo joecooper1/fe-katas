8b47f8dd27d15da3f93bf9bb19596d22eb0c9941
Joe Cooper <joecooper2991@gmail.com>
2020-05-18 14:49:51 +0100

    solved crackCode

--- __tests__/crackCode.test.js
+++ __tests__/crackCode.test.js
@@ -1,7 +1,13 @@
 const { crackCode } = require('../crackCode')
 
 describe('crackCode()', () => {
-  it('', () => {
-    // first test here
-  })
+  it('passes correct tests', () => {
+    expect(crackCode('ddd-aaa-z-y-x-123(adxy)')).toBe(true);
+    expect(crackCode('a-b-c-d-e-f-g-h-i-577(abcd)')).toBe(true);
+  });
+  it('passes incorrect tests', () => {
+    expect(crackCode('fff-gg-xx-ss-y(fgsy)')).toBe(false);
+    expect(crackCode('fff-gg-xx-ss-y(hjy7)')).toBe(false);
+    expect(crackCode('aa-bb-cc-dd-e-f(abdc)')).toBe(false);
+  });
 })
--- crackCode.js
+++ crackCode.js
@@ -1,6 +1,45 @@
 // Please do not change the name of this function
 const crackCode = (encryptedKey) => {
-  // Your code here
-}
+  //1 - check if letters in brackets are in alphabetical order
+  const lettersInBrackets = encryptedKey.slice(-5, -1);
+  if (lettersInBrackets.split("").sort().join("") !== lettersInBrackets)
+    return false;
+  else {
+    //2 - check if they are all contained in list of four or more of most common letters
+    const letterFrequencies = {};
+    const mostCommonLetters = [];
+    const eligibleLetters = [];
+    //Create object showing how many of each letter there is before brackets
+    for (let i = 0; i < encryptedKey.length - 6; i++) {
+      const letter = encryptedKey[i];
+      if (!/[a-zA-z]/.test(letter)) continue;
+      else {
+        if (letterFrequencies.hasOwnProperty(letter))
+          letterFrequencies[letter] += 1;
+        else letterFrequencies[letter] = 1;
+      }
+    }
+    //Create array of letters in order of frequency
+    for (let letter in letterFrequencies) {
+      mostCommonLetters.push(letter);
+    }
+    mostCommonLetters.sort((a, b) => {
+      return letterFrequencies[b] - letterFrequencies[a];
+    });
+    //Create array of letters with frequency equal to or higher than the fourth most frequent letter
+    for (let i in mostCommonLetters) {
+      if (
+        letterFrequencies[mostCommonLetters[i]] >=
+        letterFrequencies[mostCommonLetters[3]]
+      )
+        eligibleLetters.push(mostCommonLetters[i]);
+    }
+    //Check if each letter in brackets is also included in elgible letters array
+    for (let i = 0; i < lettersInBrackets.length; i++) {
+      if (!eligibleLetters.includes(lettersInBrackets[i])) return false;
+    };
+  }
+  return true;
+};
 
-module.exports = { crackCode }
+module.exports = { crackCode };
