c32b9c0ef32dff0df0e5502db2dafd099bdbc4c4
Joe Cooper <joecooper2991@gmail.com>
2020-05-19 12:02:33 +0100

    solved findPartner

--- __tests__/findPartner.test.js
+++ __tests__/findPartner.test.js
@@ -1,7 +1,24 @@
-const { findPartner } = require('../findPartner')
+const { findPartner } = require("../findPartner");
 
-describe('findPartner()', () => {
-  it('', () => {
-    // first test here
-  })
-})
+const students = [
+  ["Sam", "Harriet", "Mitch", "Jonny", "JD", "Kavita"],
+  ["Amul", "Chris", "Mauro", "Ruth", "Haz", "Joshua"],
+];
+
+describe("findPartner()", () => {
+  it("finds your partner if given a single input", () => {
+    expect(findPartner(students, ["right"])).toEqual(["Harriet"]);
+  });
+  it("finds your partners if given multiple inputs", () => {
+    expect(findPartner(students, ["right", "right"])).toEqual([
+      "Harriet",
+      "Mitch",
+    ]);
+  });
+  it("loops horizontally", () => {
+    expect(findPartner(students, ["left"])).toEqual(["Kavita"]);
+  });
+  it("does not loop vertically", () => {
+    expect(findPartner(students, ["right", "up", "up"])).toEqual(["Harriet"]);
+  });
+});
--- findPartner.js
+++ findPartner.js
@@ -1,6 +1,35 @@
 // Please do not change the name of this function
 const findPartner = (students, directions) => {
-  // Your code here
-}
+  const studentsMovedOver = [];
+  //Define x and y, where x is the column and y is the row
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < directions.length; i++) {
+    switch (directions[i]) {
+      case "right":
+        x += 1;
+        if (x >= students[0].length) x = 0;
+        studentsMovedOver.push(students[y][x]);
+        break;
+      case "left":
+        x -= 1;
+        if (x < 0) x = students[0].length - 1;
+        studentsMovedOver.push(students[y][x]);
+        break;
+      case "up":
+        if (y === 1) {
+          y -= 1;
+          studentsMovedOver.push(students[y][x]);
+        }
+        break;
+      case "down":
+        if (y === 0) {
+          y += 1;
+          studentsMovedOver.push(students[y][x]);
+        }
+    }
+  }
+  return studentsMovedOver;
+};
 
-module.exports = { findPartner }
+module.exports = { findPartner };
