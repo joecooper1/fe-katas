a9c7dd82133f0ea71682272d2ffa451f1b55b85b
Joe Cooper <joecooper2991@gmail.com>
2020-05-20 17:04:34 +0100

    solved wrongWayCow

--- __tests__/wrongWayCow.test.js
+++ __tests__/wrongWayCow.test.js
@@ -1,7 +1,24 @@
 const { wrongWayCow } = require('../wrongWayCow')
 
 describe('wrongWayCow()', () => {
-  it('', () => {
-    // first test here
-  })
+  it('returns no coordinates if the cows are all the same way', () => {
+    const field = 'cowcowcowcow';
+    expect(wrongWayCow(field)).toEqual([]);
+  });
+  it('returns no coordinates if field is empty', () => {
+    const field = '......';
+    expect(wrongWayCow(field)).toEqual([]);
+  });
+  it('returns coordinates of wrong way cow in field of single row', () => {
+    const field = 'cowcowcowwoc';
+    expect(wrongWayCow(field)).toEqual([0, 3]);
+  });
+  it('works if cows are facing the other way', () => {
+    const field = 'wocwoccowwoc';
+    expect(wrongWayCow(field)).toEqual([0, 2]);
+  });
+  it('works for fields with multiple rows', () => {
+    const field = 'cowcowcow...\n...woccowcow\n............';
+    expect(wrongWayCow(field)).toEqual([1,1]);
+  });
 })
--- wrongWayCow.js
+++ wrongWayCow.js
@@ -1,6 +1,28 @@
 // Please do not change the name of this function
 const wrongWayCow = (field) => {
-  // Your code here
-}
+  const fieldArray = field.split("\n");
+  //Set numbers of cows/wocs and coords of last of each
+  let cowCoords = [0, []];
+  let wocCoords = [0, []];
+  for (let i = 0; i < fieldArray.length; i++) {
+    let fieldRow = fieldArray[i].match(/.{1,3}/g);
+    fieldArray[i] = fieldRow;
+    for (let j = 0; j < fieldRow.length; j++) {
+      if (fieldRow[j] === "woc") {
+        wocCoords[0] += 1;
+        wocCoords[1] = [i, j];
+      };
+      if (fieldRow[j] === "cow") {
+        cowCoords[0] += 1;
+        cowCoords[1] = [i, j];
+      };
+    }
+  }
+  //If number of cows and wocs are both zero return empty array
+  if (cowCoords[0] === 0 || wocCoords[0] === 0) return [];
+  //Else return coords of type with least occurences
+  if (cowCoords[0] > wocCoords[0]) return wocCoords[1];
+  else return cowCoords[1];
+};
 
-module.exports = { wrongWayCow }
+module.exports = { wrongWayCow };
