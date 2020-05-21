8f34dfd4c905ad25ff56831438e092e13b4504e5
Joe Cooper <joecooper2991@gmail.com>
2020-05-21 12:19:45 +0100

    solved vowelShift

--- __tests__/vowelShift.test.js
+++ __tests__/vowelShift.test.js
@@ -1,7 +1,23 @@
 const { vowelShift } = require('../vowelShift')
 
 describe('vowelShift()', () => {
-  it('', () => {
-    // first test here
-  })
+  it('does not change word with only consonants', () => {
+    expect(vowelShift('fly', 1)).toBe('fly');
+  });
+  it('does not change word with one vowel', () => {
+    expect(vowelShift('cow', 1)).toBe('cow');
+  });
+  it('swaps vowels of words with two vowels', () => {
+    expect(vowelShift('hello', 1)).toBe('holle');
+  });
+  it('works across multiple words', () => {
+    expect(vowelShift('hello world', 1)).toBe('holle world');
+  });
+  it('works if n is greater than 1', () => {
+    expect(vowelShift('funnily enough', 4)).toBe('finnely onuugh');
+  });
+  it('works if n is negative', () => {
+    expect(vowelShift('moleman', -1)).toBe('melamon');
+    expect(vowelShift('moleman', 1)).toBe('malomen');
+  });
 })
--- vowelShift.js
+++ vowelShift.js
@@ -1,6 +1,34 @@
 // Please do not change the name of this function
-const vowelShift = (str) => {
-  // Your code here
+const vowelShift = (str, n) => {
+  //Extract vowels into array
+  const vowels = str.match(/[aeiou]/g);
+
+  //If there aren't any, return the string as is
+  if (!vowels) return str;
+
+  //Move vowels to the right n number of times
+  for (let i = 0; i < n; i++) {
+    vowels.unshift(vowels.pop());
+  };
+  //Move vowels to left if n is negative
+  for (let i = 0; i > n; i--) {
+    vowels.push(vowels.shift());
+  };
+
+  //Loop through str
+  let newStr = str;
+  let vowelNumber = 0;
+  for (let i = 0; i < str.length; i++) {
+    //If it's a vowel
+    if (['a', 'e', 'i', 'o', 'u'].indexOf(str[i].toLowerCase()) !== -1) {
+      //Replace vowel with vowel from array
+      newStr = newStr.substr(0, i) + vowels[vowelNumber] + newStr.substr(i + 1);
+      //Increment vowelNumber
+      vowelNumber++;
+    };
+  };
+  
+  return newStr;
 }
 
 module.exports = { vowelShift }
